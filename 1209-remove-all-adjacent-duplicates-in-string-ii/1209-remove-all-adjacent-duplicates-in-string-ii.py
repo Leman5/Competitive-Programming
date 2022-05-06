67c8f515664922b7e7124cfc25cfee17484d02a3
Leman5 <leman5kirme5@gmail.com>
2022-05-06 20:00:03 +0530

    Time: 128 ms (73.66%), Space: 18.7 MB (69.77%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.py
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.py
@@ -9,19 +9,9 @@ class Solution:
                 
             if stack[-1][1] == k:
                 stack.pop()
+            # print(stack)
         
         result = ""
         for char, count in stack:
             result += (char * count)
-        
         return result
-        # if(len(s) <k):
-        #     return s
-        # for i in range(len(s)-k+1):
-        #     for j in range(i+k-1,i-1,-1):
-        #         if(s[i]!=s[j] ):
-        #             break
-        #         if(j == i+k-1):
-        #             print(s[:i]+s[i+k:],len(s[:i]+s[i+k:]))
-        #             return self.removeDuplicates(s[:i]+s[i+k:], k)
-        # return s
