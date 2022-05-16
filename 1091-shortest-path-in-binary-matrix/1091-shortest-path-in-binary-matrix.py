80609efd4c1a3fcb542717304f9d0c1ce0e0bd12
Leman5 <leman5kirme5@gmail.com>
2022-05-16 07:09:16 +0530

    Time: 780 ms (59.11%), Space: 14.2 MB (95.32%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -1,18 +1,16 @@
 from collections import deque
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        que = deque([(1,(0,0))])
-        m, n = len(grid), len(grid[0])
-        visited =set()
-        while que:
-            d, (i, j) = que.popleft()
-            if grid[i][j] == 1: continue
-            if (i,j) in visited: continue
-            if (i,j) == (m-1,n-1):
-                return d
-            visited.add((i,j))
-            for di, dj in [[-1,0],[1,0],[0,-1],[0,1],[-1,1],[1,1],[1,-1],[-1,-1]]:
-                if 0<=i+di<len(grid) and 0<=j+dj<len(grid[0]):
-                    que.append((d+1,(i+di,j+dj)))
+        if grid[0][0] or grid[-1][-1]:
+            return -1
+        queue = collections.deque([(0,0,1)])
+        while queue:
+            i, j, count = queue.popleft()
+            if i == len(grid)-1 and j == len(grid[0])-1:
+                return count
+            for x, y in ((0,1), (0,-1), (1,0), (-1,0), (1,-1), (-1,1), (1,1), (-1,-1)):
+                row, col = i + y, j + x
+                if 0 <= row < len(grid) and 0 <= col < len(grid[0]) and not grid[row][col]:
+                    queue.append((row, col, count+1))
+                    grid[row][col] = 1
         return -1
-        
