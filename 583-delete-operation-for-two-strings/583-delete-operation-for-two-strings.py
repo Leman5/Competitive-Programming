0ce1be5eb6b338e62869e01b8e8d276117893b4e
Leman5 <leman5kirme5@gmail.com>
2022-06-14 08:07:25 +0530

    Time: 417 ms (43.21%), Space: 17.5 MB (47.65%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.py
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.py
@@ -9,15 +9,12 @@ class Solution:
                 dp[i][j] = 0
                 return 0
             if(dp[i][j]=='#'):
-                # print(word1[:i+1],word2[:j+1])
                 if(word1[i-1]==word2[j-1]):
                     dp[i][j] = long(i-1,j-1)+1
                 else:
                     dp[i][j] = max(long(i-1,j),long(i,j-1))
             return dp[i][j]
         lcs = long(n,m)
-        # print(lcs)
-        # print(dp)
         out = m-lcs
         out += n-lcs
         return out
