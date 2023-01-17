f591c1c24058167f3f2676a3cac731869d7662b2
Leman5 <leman5kirme5@gmail.com>
2023-01-17 13:23:50 +1100

    Added solution - LeetHub

---
+++ Replace all 0's with 5 - GFG/replace-all-0s-with-5.py
@@ -0,0 +1,20 @@
+#function should return an integer
+#your task is to compplete this function
+class Solution:
+    def convertFive(self,n):
+        #Code here
+        n = str(n)
+        n = n.replace('0','5')
+        return int(n)
+
+
+#{ 
+ # Driver Code Starts
+if __name__=='__main__':
+    t = int(input())
+    for i in range(t):
+        n=int(input())
+        print(Solution().convertFive(n))
+# Contributed by: Harshit sidhwa
+
+# } Driver Code Ends
