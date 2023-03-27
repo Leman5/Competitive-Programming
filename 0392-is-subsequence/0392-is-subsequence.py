4d369a66de9cd4e36e954362c22a0dcc443c9ff0
Leman5 <leman5kirme5@gmail.com>
2023-03-27 17:39:30 +1100

    Time: 24 ms (97.32%), Space: 13.9 MB (31.87%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -1,18 +1,30 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        sn = len(s)
-        tn = len(t)
-        dp = [[False]*(tn+1)]*(sn+1)
-        def check(i,j):
+        i = len(s)
+        j = len(t)
+        # dp = [[False]*(tn+1)]*(sn+1)
+        # def check(i,j):
+        #     if(i==0):
+        #         return True
+        #     if(i>j) :
+        #         return False
+        #     if(dp[i][j]):
+        #         return dp[i][j]
+        #     if(s[i-1] ==t[j-1] ):
+        #         dp[i][j] = check(i-1,j-1)
+        #     else:
+        #         dp[i][j] = check(i,j-1)
+        #     return dp[i][j]
+        # return check(sn,tn)
+        while(i<=j):
             if(i==0):
                 return True
-            if(i>j) :
-                return False
-            if(dp[i][j]):
-                return dp[i][j]
-            if(s[i-1] ==t[j-1] ):
-                dp[i][j] = check(i-1,j-1)
+            if(s[i-1]==t[j-1]):
+                i -= 1
+                j -= 1
             else:
-                dp[i][j] = check(i,j-1)
-            return dp[i][j]
-        return check(sn,tn)
+                j -= 1
+        if(i<=0):
+            return True
+        else:
+            return False
