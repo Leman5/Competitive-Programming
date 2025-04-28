c1d36a9fbdb3c7badd8e03b550cf754d0360ad48
Leman5 <leman5kirme5@gmail.com>
2025-04-28 19:37:36 +1000

    Time: 8 ms (23.31%) | Memory: 19.8 MB (22.95%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,18 +1,27 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        nums.sort()
-        for i in range(0, len(nums)-1, 2):
-            # print("h")
-            if(nums[i]!=nums[i+1]):
-                return nums[i]
-            # i+=1
-        return nums[-1]
+        # nums.sort()
+        # for i in range(0, len(nums)-1, 2):
+        #     if(nums[i]!=nums[i+1]):
+        #         return nums[i]
+        # return nums[-1]
 
         # Solution 1:
         #     make a dict:
         #         if that number ka key not  present add it and value is false
         #         if that number ka key already present make the value as true
         #     find the false value in dict
+
+        d = dict()
+        for num in nums:
+            if num in d.keys():
+                d[num] = True
+            else:
+                d[num] = False
+        for key,value in d.items():
+            if value == False:
+                return key
+        
 
 
         # Solution 2: 
